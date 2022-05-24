1796037ad38d415a839f3db6ae93bb28b03fc871
Oscar Norman <Oscar.Norman@primekey.com>
2022-05-24 13:35:53 +0200

    WIP config panel

--- SortingVisualizer/src/SortVisualizer/MenuPanel.java
+++ SortingVisualizer/src/SortVisualizer/MenuPanel.java
@@ -5,9 +5,16 @@ import java.awt.*;
 
 public class MenuPanel extends JPanel {
 
+    private String[] sortingAlgorithms = {"Bubble Sort, Heap Sort, Insertion Sort, Merge Sort"};
+    private JComboBox comboBox = new JComboBox(sortingAlgorithms);
+
     public MenuPanel() {
         this.setPreferredSize(new Dimension(1200, 50));
         this.setBackground(new Color(152, 156, 149));
+        this.add(new JButton("Knapp Ett"));
+        this.add(comboBox);
+        this.add(new JSlider());
+        this.add(new JButton("Start"));
         this.setVisible(true);
     }
 
